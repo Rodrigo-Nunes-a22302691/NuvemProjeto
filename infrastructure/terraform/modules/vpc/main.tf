9e632b52f9ae7ead5bdeb478d8d4b4ffd774e8d5
rodri <a22302691@alunos.ulht.pt>
2026-05-30 20:09:32 +0100

    Teste das instancias AWS

--- infrastructure/terraform/modules/vpc/main.tf
+++ infrastructure/terraform/modules/vpc/main.tf
@@ -94,6 +94,14 @@ resource "aws_security_group" "web" {
   }
 
   ingress {
+    description = "Microservices"
+    from_port   = 8080
+    to_port     = 8083
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     description = "HTTPS"
     from_port   = 443
     to_port     = 443
